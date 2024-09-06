8568f39a076a6bf52ba9b3b49c503c08cce578cf
Ned_Chow <53514723+Nedasd@users.noreply.github.com>
2024-09-06 12:53:02 +0200

    Update the run.sh, use job array to reduce the time

--- plotting/network_warning/run.sh
+++ plotting/network_warning/run.sh
@@ -1,19 +1,40 @@
 #!/bin/bash
-#SBATCH -t 4-00:00:00              # time limit: (D-HH:MM:SS) 
+#SBATCH -t 4-00:00:00              # time limit: (D-HH:MM:SS)
 #SBATCH --job-name=warning         # job name, "Qi_run"
 
 #SBATCH --ntasks=1                 # each individual task in the job array will have a single task associated with it
-#SBATCH --array=1-1               # job array id
+#SBATCH --array=1-9                # job array id
 
-#SBATCH --mem-per-cpu=24G		       # Memory Request (per CPU; can use on GLIC)
+#SBATCH --mem-per-cpu=8G		       # Memory Request (per CPU; can use on GLIC)
 #SBATCH --begin=2023-10-20T09:00:00# job start time, if it later than NOW, job will be run immediatly.
 
-#SBATCH --output /home/qizhou/3paper/2AGU_revise/ML-BL-enhance-DF-EWs/plotting/network_warning/logs/out.txt 		# Standard Output Log File (for Job Arrays)
-#SBATCH --error  /home/qizhou/3paper/2AGU_revise/ML-BL-enhance-DF-EWs/plotting/network_warning/logs/err.txt 		# Standard Error Log File (for Job Arrays)
+#SBATCH --output /home/qizhou/3paper/2AGU_revise/ML-BL-enhance-DF-EWs/plotting/network_warning/logs/out_%A_%a_%x.txt 		# Standard Output Log File (for Job Arrays)
+#SBATCH --error  /home/qizhou/3paper/2AGU_revise/ML-BL-enhance-DF-EWs/plotting/network_warning/logs/err_%A_%a_%x.txt 		# Standard Error Log File (for Job Arrays)
 
 
 source /home/qizhou/miniforge3/bin/activate
 conda activate seismic
 
+
+
+parameters1=("Random_Forest" "XGBoost" "LSTM") # input station
+parameters2=("A" "B" "C") # input model
+parameters3=("EHZ") # input features
+
+
+# Calculate the indices for the current combination
+parameters1_idx=$(( ($SLURM_ARRAY_TASK_ID - 1) / ( ${#parameters2[@]} * ${#parameters3[@]} ) % ${#parameters1[@]} + 1 ))
+parameters2_idx=$(( ($SLURM_ARRAY_TASK_ID - 1) / ${#parameters3[@]} % ${#parameters2[@]} + 1 ))
+parameters3_idx=$(( ($SLURM_ARRAY_TASK_ID - 1) % ${#parameters3[@]} + 1 ))
+
+# Get the current parameter values
+current_parameters1=${parameters1[$parameters1_idx - 1]}
+current_parameters2=${parameters2[$parameters2_idx - 1]}
+current_parameters3=${parameters3[$parameters3_idx - 1]}
+
+
 # Run your Python script using srun with the parameters
-srun python /home/qizhou/3paper/2AGU_revise/ML-BL-enhance-DF-EWs/plotting/network_warning/warning_strategy.py
+srun python /home/qizhou/3paper/2AGU_revise/ML-BL-enhance-DF-EWs/plotting/network_warning/warning_strategy.py \
+     --model_type "$current_parameters1" \
+     --feature_type "$current_parameters2" \
+     --input_component "$current_parameters3"
--- plotting/network_warning/vasulize_network_warning.py
+++ plotting/network_warning/vasulize_network_warning.py
@@ -17,60 +17,151 @@ import matplotlib.pyplot as plt
 import matplotlib.ticker as ticker
 from obspy import UTCDateTime
 import matplotlib.gridspec as gridspec
+import matplotlib.colors as mcolors
+from mpl_toolkits.axes_grid1.axes_divider import make_axes_locatable
+
 
 plt.rcParams.update( {'font.size':7, 'font.family': "Arial"} )#, 'font.weight':'bold'
-
 parent_dir = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))  # get the parent path
-df = pd.read_csv(f"{parent_dir}/plotting/network_warning/warning_summary.txt", header=None)
-arr = np.array(df)
 
-model_type_arr, feature_type_arr = arr[:, 0], arr[:, 1]
-model_type_arr, feature_type_arr = np.char.strip(model_type_arr.astype(str)),np.char.strip(feature_type_arr.astype(str))
+
+def create_discrate_cmap(total_num, max_value, interval=10):
+    cmap = sns.color_palette('Oranges', total_num)  # Get 10 shades of blue
+    cmap = mcolors.ListedColormap(cmap)  # Create a discrete colormap
+
+    bounds = np.linspace(0, max_value, interval+1)  # 10 intervals between 0 and 150
+    norm = mcolors.BoundaryNorm(bounds, cmap.N)
+
+    return cmap, bounds, norm
+
+
+def convert2table(x, y, value):
+    df = pd.DataFrame({
+        'x': x.astype(float),
+        'y': np.round(y.astype(float),1),
+        'value': value.astype(float)
+    })
+    pivot_table = df.pivot(index='y', columns='x', values='value')
+
+    return np.around(np.array(pivot_table))
+
+
+def fetch_data(model_type, feature_type, data_type):
+
+    parent_dir = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))  # get the parent path
+    df = pd.read_csv(f"{parent_dir}/plotting/network_warning/warning_summary.txt", header=None)
+    arr = np.array(df)
+
+    model_type_arr, feature_type_arr = arr[:, 0], arr[:, 1]
+    model_type_arr = np.char.strip(model_type_arr.astype(str))
+    feature_type_arr = np.char.strip(feature_type_arr.astype(str))
+
+    id1 = np.where(model_type_arr == model_type)
+    id2 = np.where(feature_type_arr == feature_type)
+
+    intersection = np.intersect1d(id1, id2)
+    intersection_arr = arr[intersection, :]
+
+    pro_threshold = intersection_arr[:, 3]  # always is 0 now 2024-09-05
+    warning_threshold = intersection_arr[:, 4]
+    attention_window_size = intersection_arr[:, 5]
+
+    total_increase_warning = intersection_arr[:, 9] / 60  # original unit is second
+    mean_increase_warning = total_increase_warning / 12
+    max_increase_warning = intersection_arr[:, 10] / 60  # original unit is second
+    min_increase_warning = intersection_arr[:, 11] / 60  # original unit is second
+
+    failed_warning_times = intersection_arr[:, 12]
+    fasle_warning_times = intersection_arr[:, 7]
+
+    if data_type == "failed_warning":
+        try:
+            id3 = np.where((failed_warning_times + fasle_warning_times) == 0)  # no false warning, no failured
+            l1 = np.where(mean_increase_warning == np.max(mean_increase_warning[id3]))
+            l2 = np.where(min_increase_warning == np.max(min_increase_warning[id3]))
+            l3 = np.intersect1d(l1, l2)
+            for idx, step in enumerate(l3):
+                record = f"{model_type, feature_type, data_type}, " \
+                         f"{idx + 1}/{len(l3)}, " \
+                         f"global pro: {intersection_arr[step, 4]}, " \
+                         f"window size: {intersection_arr[step, 5]}, " \
+                         f"total increased: {intersection_arr[step, 9] / 60}, " \
+                         f"min increased: {intersection_arr[step, 11] / 60}"
+                print(record)
+
+        except Exception as e:
+            print(f"{model_type, feature_type, data_type}, {e}")
+    else:
+        pass
+
+
+    if data_type == "min":
+        pivot_table = convert2table(attention_window_size, warning_threshold, min_increase_warning)
+    elif data_type == "mean":
+        pivot_table = convert2table(attention_window_size, warning_threshold, mean_increase_warning)
+    elif data_type == "failed_warning":
+        pivot_table = convert2table(attention_window_size, warning_threshold, failed_warning_times)
+    elif data_type == "fasle_warning_times":
+        pivot_table = convert2table(attention_window_size, warning_threshold, fasle_warning_times)
+    else:
+        print(f"check the data type {data_type}")
+
+    return pivot_table
+
+
+def plot_network_warning(model_type, feature_type):
+
+    fig = plt.figure(figsize=(5.5, 3))
+    gs = gridspec.GridSpec(1, 2)
+
+    ax = plt.subplot(gs[0])
+    pivot_table = fetch_data(model_type, feature_type, "failed_warning")
+    cmap, bounds, norm = create_discrate_cmap(10, 150)
+    ax = sns.heatmap(pivot_table, annot=pivot_table, cmap='Oranges', cbar=False)
+
+    plt.xticks([0.5, 4.5, 9.5, 14.5, 19.5], [1, 5, 10, 15, 20])
+    plt.yticks(np.arange(0.5, 10, 1), np.round(np.arange(0.1, 1.1, 0.1), 1), horizontalalignment="right", rotation=0)
+
+    plt.xlabel(r"Attention window size $l$ (minute)")
+    plt.ylabel(r"Warning Threshold $Pr_{g}$")
+
+    divider = make_axes_locatable(ax)
+    cax = divider.append_axes("bottom", size="3%", pad=0.4)
+    cbar = plt.colorbar(mappable=ax.get_children()[0], cax=cax, orientation="horizontal")
+    cbar.ax.xaxis.set_ticks_position('bottom')
+    cbar.set_label("Failured Warning Event", labelpad=6)
+
+    ax = plt.subplot(gs[1])
+    pivot_table = fetch_data(model_type, feature_type, "fasle_warning_times")
+
+    print(np.max(np.array(pivot_table)))
+    cmap = sns.color_palette('Oranges', 6)
+    cmap = mcolors.ListedColormap(cmap)
+    bounds = np.array([0, 5, 10, 100, 200, 1100])  # np.linspace(0, 1100, 5)
+    norm = mcolors.BoundaryNorm(bounds, cmap.N)
+
+    label = np.where(pivot_table > 10, "", pivot_table.astype(int))
+
+    ax = sns.heatmap(pivot_table, annot=label, cmap=cmap, norm=norm, cbar=False, fmt='', cbar_kws={"ticks": bounds})
+
+    plt.xticks([0.5, 4.5, 9.5, 14.5, 19.5], [1, 5, 10, 15, 20])
+    plt.yticks(np.arange(0.5, 10, 1), np.round(np.arange(0.1, 1.1, 0.1), 1), horizontalalignment="right", rotation=0)
+
+    plt.xlabel(r"Attention window size $l$ (minute)")
+    plt.ylabel("")
+
+    divider = make_axes_locatable(ax)
+    cax = divider.append_axes("bottom", size="3%", pad=0.4)
+    cbar = plt.colorbar(mappable=ax.get_children()[0], cax=cax, orientation="horizontal")
+    cbar.ax.xaxis.set_ticks_position('bottom')
+    cbar.set_label("Failured Warning Event", labelpad=6)
+
+    plt.tight_layout()
+    plt.savefig(f"{parent_dir}/plotting/network_warning/fig/{model_type}-{feature_type}.png", dpi=600)
+    plt.show()
+
 
 
 for model_type in ["Random_Forest", "XGBoost", "LSTM"]:
     for feature_type in ["A", "B", "C"]:
-
-        id1 = np.where(model_type_arr == model_type)
-        id2 = np.where(feature_type_arr == feature_type)
-
-        intersection = np.intersect1d(id1, id2)
-        intersection_arr = arr[intersection, :]
-
-        pro_threshold = intersection_arr[:, 3] # always is 0 now 2024-09-05
-        warning_threshold = intersection_arr[:, 4]
-        attention_window_size = intersection_arr[:, 5]
-
-        total_increase_warning = intersection_arr[:, 7] / 60 # original unit is second
-        mean_increase_warning = total_increase_warning / 12
-        max_increase_warning = intersection_arr[:, 8] / 60 # original unit is second
-        min_increase_warning = intersection_arr[:, 9] / 60 # original unit is second
-        failed_warning = intersection_arr[:, 10]
-
-        # Create a DataFrame from the arrays
-        df1 = pd.DataFrame({
-            'x': attention_window_size.astype(float),
-            'y': np.round(warning_threshold.astype(float), 1),
-            'value': total_increase_warning.astype(float)
-        })
-        pivot_table1 = df1.pivot(index='y', columns='x', values='value')
-
-        df2 = pd.DataFrame({
-            'x': attention_window_size.astype(float),
-            'y': np.round(warning_threshold.astype(float), 1),
-            'value': failed_warning.astype(float)
-        })
-        pivot_table2 = df2.pivot(index='y', columns='x', values='value')
-
-
-        sns.heatmap(pivot_table1, annot=pivot_table2)
-
-        plt.xlabel("attention_window_size (minute)")
-        plt.ylabel("warning_threshold")
-        plt.title(f"{model_type}-{feature_type}")
-        plt.tight_layout()
-        plt.savefig(f"{parent_dir}/plotting/network_warning/{model_type}-{feature_type}.png", dpi=600)
-        plt.show()
-
-
-
+        plot_network_warning(model_type, feature_type)
