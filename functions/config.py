2dae09b87c32e1d7a2836ed49deed38762c84857
Ned_Chow <53514723+Nedasd@users.noreply.github.com>
2024-09-22 22:38:28 +0200

    Add the config.py for file dir

---
+++ functions/config.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+#__modification time__ = 2024-09-22
+#__author__ = Qi Zhou, Helmholtz Centre Potsdam - GFZ German Research Centre for Geosciences
+#__find me__ = qi.zhou@gfz-potsdam.de, qi.zhou.geo@gmail.com, https://github.com/Nedasd
+# Please do not distribute this code without the author's permission
+
+CONFIG = {
+    "input_file": "data/input.txt",
+    "output_file": "data/output.txt"
+}
