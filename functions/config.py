ddfb5c03efb918a47db5fc5b0d7a461ef78c6e7a
Ned_Chow <53514723+Nedasd@users.noreply.github.com>
2024-09-23 09:07:33 +0200

    Add the config.py to set the file input-output path

--- functions/config.py
+++ functions/config.py
@@ -6,7 +6,17 @@
 #__find me__ = qi.zhou@gfz-potsdam.de, qi.zhou.geo@gmail.com, https://github.com/Nedasd
 # Please do not distribute this code without the author's permission
 
-CONFIG = {
-    "input_file": "data/input.txt",
-    "output_file": "data/output.txt"
-}
+import os
+
+def config_dir():
+    parent_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))  # get the parent path
+    output_dir = f"{parent_dir}/output"
+
+    CONFIG_dir = {
+        "parent_dir": parent_dir,
+        "output_dir": output_dir
+    }
+
+    return CONFIG_dir
+
+CONFIG_dir = config_dir()
