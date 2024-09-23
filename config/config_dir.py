0f661b7b98259905abc23c809ab30a5b97fa4557
Ned_Chow <53514723+Nedasd@users.noreply.github.com>
2024-09-23 10:56:40 +0200

    Update, add sac path for 1A network

--- config/config_dir.py
+++ config/config_dir.py
@@ -11,10 +11,16 @@ import os
 def config_dir():
     parent_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))  # get the parent path
     output_dir = f"{parent_dir}/output"
+    sac_path_Illgraben = f"/storage/vast-gfz-hpc-01/project/seismic_data_qi/seismic/EU/Illgraben/"
+    sac_path_Museum = f"/storage/vast-gfz-hpc-01/project/seismic_data_qi/seismic/USA/Museum_Fire/"
+    sac_path_Luding = "/storage/vast-gfz-hpc-01/project/seismic_data_qi/seismic/CN/Luding"
 
     CONFIG_dir = {
         "parent_dir": parent_dir,
-        "output_dir": output_dir
+        "output_dir": output_dir,
+        "sac_path_Illgraben": sac_path_Illgraben,
+        "sac_path_Museum": sac_path_Museum,
+        "sac_path_Luding": sac_path_Luding
     }
 
     return CONFIG_dir
