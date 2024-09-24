1ddd38e5a943a2c0b78900e7602c57a6cccd4cf7
Ned_Chow <53514723+Nedasd@users.noreply.github.com>
2024-09-24 18:02:24 +0200

    Add， 2022 9S feature calculate sbatch

---
+++ calculate_features/2022/submitStep3.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+#SBATCH -t 4-00:00:00              # time limit: (D-HH:MM:SS) 
+#SBATCH --job-name=step3           # job name, "Qi_run"
+
+#SBATCH --ntasks=1                 # each individual task in the job array will have a single task associated with it
+#SBATCH --array=1-3                # job array id
+
+#SBATCH --mem-per-cpu=8G		   # Memory Request (per CPU; can use on GLIC)
+
+#SBATCH --chdir=/home/qizhou/3paper/2AGU_revise/ML-BL-enhance-DF-EWs/calculate_features # set working dir
+#SBATCH --output=2022/logs/out_%A_%a_%x.txt  # Standard Output Log File
+#SBATCH --error=2022/logs/err_%A_%a_%x.txt   # Standard Error Log File
+
+
+source /home/qizhou/miniforge3/bin/activate
+conda activate seismic
+
+# Define arrays for parameters1, parameters2, and parameters3
+parameters1=(2022)
+parameters2=("ILL17" "ILL12" "ILL13")
+parameters3=("EHZ")
+
+# Calculate the indices for the current combination
+parameters1_idx=$(( ($SLURM_ARRAY_TASK_ID - 1) / ( ${#parameters2[@]} * ${#parameters3[@]} ) % ${#parameters1[@]} + 1 ))
+parameters2_idx=$(( ($SLURM_ARRAY_TASK_ID - 1) / ${#parameters3[@]} % ${#parameters2[@]} + 1 ))
+parameters3_idx=$(( ($SLURM_ARRAY_TASK_ID - 1) % ${#parameters3[@]} + 1 ))
+
+# Get the current parameter values
+current_parameters1=${parameters1[$parameters1_idx - 1]}
+current_parameters2=${parameters2[$parameters2_idx - 1]}
+current_parameters3=${parameters3[$parameters3_idx - 1]}
+
+# Print the current combination
+echo "Year: $current_parameters1, Station: $current_parameters2, Component: $current_parameters3"
+
+# Run your Python script using srun with the parameters
+srun python 3merge_single_julday.py \
+    --input_year "$current_parameters1" \
+    --input_station "$current_parameters2" \
+    --input_component "$current_parameters3" \
+    --id1 150 \
+    --id2 273
---
+++ functions/issue_network_warning/main.py
@@ -0,0 +1,61 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+#__modification time__ = 2024-02-23
+#__author__ = Qi Zhou, Helmholtz Centre Potsdam - GFZ German Research Centre for Geosciences
+#__find me__ = qi.zhou@gfz-potsdam.de, qi.zhou.geo@gmail.com, https://github.com/Nedasd
+# Please do not distribute this code without the author's permission
+
+import os
+import sys
+import argparse
+
+import pandas as pd
+import numpy as np
+from obspy.core import UTCDateTime # default is UTC+0 time zone
+from datetime import datetime
+
+
+# Get the absolute path of the parent directory
+parent_dir = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
+if parent_dir not in sys.path:
+    sys.path.append(parent_dir)
+
+# internal functions
+from config.config_dir import CONFIG_dir
+from functions.issue_network_warning.warning_strategy import *
+
+def main(model_type, feature_type, input_component):
+
+    print(model_type, feature_type, input_component)
+
+    pro_threshold = 0
+    input_station_list = ["ILL18", "ILL12", "ILL13"]
+
+    for idx1, warning_threshold in enumerate(np.arange(0.1, 1.1, 0.1)):
+        for idx2, attention_window_size in enumerate(np.arange(1, 21, 1)):
+
+            warning_threshold = np.round(warning_threshold, 1)
+            attention_window_size = np.round(attention_window_size, 0)
+
+            warning(pro_threshold, warning_threshold, attention_window_size,
+                    input_station_list, model_type, feature_type, input_component)
+
+            record = warning_summary(pro_threshold, warning_threshold, attention_window_size,
+                                     model_type, feature_type, input_component)
+
+            print(f"Finish, {idx1}--{idx2}, {record} {pro_threshold, warning_threshold, attention_window_size, model_type, feature_type, input_component}",
+                  datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='input parameters')
+
+    parser.add_argument("--model_type", default="Random_Forest", type=str, help="model type")
+    parser.add_argument("--feature_type", default="C", type=str, help="feature type")
+    parser.add_argument("--input_component", default="EHZ", type=str, help="seismic input_component")
+
+    args = parser.parse_args()
+
+    main(args.model_type, args.feature_type, args.input_component)
+
