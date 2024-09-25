8385bf65bd17a1624f783b1bc3d652fca2060f5b
Ned_Chow <53514723+Nedasd@users.noreply.github.com>
2024-09-25 14:31:58 +0200

    Update the SBATCH --output

--- functions/issue_network_warning/run_main.sh
+++ functions/issue_network_warning/run_main.sh
@@ -8,13 +8,13 @@
 #SBATCH --mem-per-cpu=8G		       # Memory Request (per CPU; can use on GLIC)
 #SBATCH --begin=2023-10-20T09:00:00# job start time, if it later than NOW, job will be run immediatly.
 
-#SBATCH --output /home/qizhou/3paper/2AGU_revise/ML-BL-enhance-DF-EWs/plotting/network_warning/logs/out_%A_%a_%x.txt 		# Standard Output Log File (for Job Arrays)
-#SBATCH --error  /home/qizhou/3paper/2AGU_revise/ML-BL-enhance-DF-EWs/plotting/network_warning/logs/err_%A_%a_%x.txt 		# Standard Error Log File (for Job Arrays)
+#SBATCH --chdir=/home/qizhou/3paper/2AGU_revise/ML-BL-enhance-DF-EWs/functions/issue_network_warning # set working dir
+#SBATCH --output=logs/out_%A_%a_%x.txt  # Standard Output Log File
+#SBATCH --error=logs/err_%A_%a_%x.txt   # Standard Error Log File
 
 
 source /home/qizhou/miniforge3/bin/activate
 conda activate seismic
-
 
 
 parameters1=("Random_Forest" "XGBoost" "LSTM") # input station
@@ -34,7 +34,7 @@ current_parameters3=${parameters3[$parameters3_idx - 1]}
 
 
 # Run your Python script using srun with the parameters
-srun python /home/qizhou/3paper/2AGU_revise/ML-BL-enhance-DF-EWs/plotting/network_warning/warning_strategy.py \
+srun python main.py \
      --model_type "$current_parameters1" \
      --feature_type "$current_parameters2" \
      --input_component "$current_parameters3"
