4f3779a1a2ab5e940c10bc4070a993611176b29b
Ned_Chow <53514723+Nedasd@users.noreply.github.com>
2024-11-26 21:55:39 +0100

    add "waveform and feature" sbatch file

---
+++ calculate_features/sbatch/Y7/submitStep1.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+#SBATCH -t 4-00:00:00              # time limit: (D-HH:MM:SS) 
+#SBATCH --job-name=step1           # job name, "Qi_run"
+
+#SBATCH --ntasks=1                 # each individual task in the job array will have a single task associated with it
+#SBATCH --array=1-627              # job array id, !!!! all combination of parameters1-3
+#SBATCH --mem-per-cpu=32G		       # Memory Request (per CPU; can use on GLIC)
+
+#SBATCH --chdir=/home/qizhou/3paper/0seismic_feature/sbatch/Y7/logs # set working dir
+#SBATCH --output=step1/out_%A_%a_%x.txt  # Standard Output Log File
+#SBATCH --error=step1/err_%A_%a_%x.txt   # Standard Error Log File
+
+
+source /home/qizhou/miniforge3/bin/activate
+conda activate seismic
+
+
+# Define arrays for parameters1, parameters2, and parameters3
+parameters1=(2009)
+parameters2=("AV11" "AV12" "AV13")
+parameters3=($(seq 156 364)) # 209 = 364 - 156 + 1
+
+
+# Calculate the indices for the current combination
+parameters1_idx=$(( ($SLURM_ARRAY_TASK_ID - 1) / ( ${#parameters2[@]} * ${#parameters3[@]} ) % ${#parameters1[@]} + 1 ))
+parameters2_idx=$(( ($SLURM_ARRAY_TASK_ID - 1) / ${#parameters3[@]} % ${#parameters2[@]} + 1 ))
+parameters3_idx=$(( ($SLURM_ARRAY_TASK_ID - 1) % ${#parameters3[@]} + 1 ))
+
+# Get the current parameter values
+current_parameters1=${parameters1[$parameters1_idx - 1]}
+current_parameters2=${parameters2[$parameters2_idx - 1]}
+current_parameters3=${parameters3[$parameters3_idx - 1]}
+
+# Print the current combination
+echo "Year: $current_parameters1, Station: $current_parameters2, Julday $current_parameters3"
+
+srun python /home/qizhou/3paper/2AGU_revise/ML-BL-enhance-DF-EWs/calculate_features/1cal_TypeA_TypeB.py \
+    --seismic_network "Y7" \
+    --input_year "$current_parameters1" \
+    --input_station "$current_parameters2" \
+    --input_component "ELZ" \
+    --input_window_size 60 \
+    --id "$current_parameters3"
